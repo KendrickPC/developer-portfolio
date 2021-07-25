568fa6f6a9e4e395492a7afd22c7ff5c7099282a
kendrickPC <kennethpchang@gmail.com>
2021-07-25 12:30:09 -0700

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,221 @@
+import emoji from "react-easy-emoji";
+
+import googlelogo from "./assets/img/icons/common/google.svg";
+import github from "./assets/img/icons/common/github.svg";
+import airbnb from "./assets/img/icons/common/airbnbLogo.png";
+
+export const greetings = {
+	name: "Kenneth P. Chang",
+	title: "Welcome to EveryDayKenneth.com",
+	description:
+		"I am a passionate Full Stack Engineer with experience in MongoDB, Express, Reactjs, Node.js and Python. Contact me if you would like to build something together. 😬 ",
+	resumeLink: "https://www.yahoo.com",
+};
+
+export const openSource = {
+	githubUserName: "KendrickPC",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	github: "https://github.com/kendrickpc",
+	linkedin: "https://www.linkedin.com/in/kenneth-chang-94569a142/",
+	facebook: "https://www.facebook.com/kenneth.f.chang/",
+	instagram: "https://www.instagram.com/brotherkendrick/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"PASSIONATE FULL STACK DEVELOPER WHO LOVES TO BUILD! ",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		emoji(
+			"⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "sass",
+			fontAwesomeClassname: "logos:sass",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		{
+			skillName: "TypeScript",
+			fontAwesomeClassname: "logos:typescript-icon",
+		},
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		{
+			skillName: "flutter",
+			fontAwesomeClassname: "logos:flutter",
+		},
+		{
+			skillName: "swift",
+			fontAwesomeClassname: "vscode-icons:file-type-swift",
+		},
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		{
+			skillName: "aws",
+			fontAwesomeClassname: "logos:aws",
+		},
+		{
+			skillName: "firebase",
+			fontAwesomeClassname: "logos:firebase",
+		},
+		{
+			skillName: "python",
+			fontAwesomeClassname: "logos:python",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+		{
+			skillName: "docker",
+			fontAwesomeClassname: "logos:docker-icon",
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Backend",
+		progressPercentage: "70",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "60",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Harvard University",
+		subHeader: "Master of Science in Computer Science",
+		duration: "September 2017 - April 2019",
+		desc: "Participated in the research of XXX and published 3 papers.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		schoolName: "Harvard",
+		subHeader: "Master of Science in Computer Science",
+		duration: "September 2017 - April 2019",
+		desc: "Participated in the research of XXX and published 3 papers.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		schoolName: "Stanford University",
+		subHeader: "Bachelor of Science in Computer Science",
+		duration: "September 2013 - April 2017",
+		desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+		descBullets: [
+			"Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit",
+		],
+	},
+];
+
+export const experience = [
+	{
+		role: "Software Engineer",
+		company: "Google",
+		companylogo: googlelogo,
+		date: "June 2018 – Present",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		role: "Front-End Developer",
+		company: "Github",
+		companylogo: github,
+		date: "May 2017 – May 2018",
+		desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+	{
+		role: "Software Engineer Intern",
+		company: "Airbnb",
+		companylogo: airbnb,
+		date: "Jan 2015 – Sep 2015",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+];
+
+export const projects = [
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		link: "https://example.com",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/kendrickpc",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/kendrickpc",
+		link: "https://example.com",
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "Naymar",
+		feedback:
+			"I wish Kenneth could play for our National team.",
+	},
+	{
+		name: "Kylian Mbappe",
+		feedback:
+			"Kenneth taught me how to run fast.",
+	},
+];
